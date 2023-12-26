97452507bee22755e06df968f08f78974a16222f
molor824 <molormargaderdene@gmail.com>
2023-12-26 19:15:40 +0800

    additional fixes of instruction methods

--- examples/for_loop.py
+++ examples/for_loop.py
@@ -1,16 +1,11 @@
 from hex_gen import *
 import numpy as np
 
-def main():
-    hexcode = np.full(0x100, NOP, dtype=np.uint32)
-    hexcode[0] = add(True, False, ZERO, ZERO, 0, 10)
-    hexcode[1] = jump_if(COND_LE, True, 0, ZERO, 10)
-    hexcode[2] = sub(True, False, 0, ZERO, 0, 1)
-    hexcode[3] = jump(False, False, -2)
-    hexcode[11] = HALT
+hexcode = np.full(0x100, NOP, dtype=np.uint32)
+hexcode[0] = add(True, False, 0, immediate=10)
+hexcode[1] = jump_if(COND_LE, True, 10, 0)
+hexcode[2] = sub(True, False, 0, 0, immediate=1)
+hexcode[3] = jump(False, -2)
+hexcode[11] = HALT
 
-    path = 'forloop.bin'
-    hexcode.byteswap().tofile(path)
-
-if __name__ == "__main__":
-    main()
+hexcode.byteswap().tofile("forloop.bin")
--- examples/hex_gen.py
+++ examples/hex_gen.py
@@ -24,20 +24,25 @@ def get_rs2(rs2: int):
 def get_rd(rd: int):
     return (int(rd) & 0xf) << 28
 
-def jump(link: bool, absolute: bool, immediate: int = 0, rs1: int = ZERO, rd: int = RJL):
+def jump(absolute: bool, immediate: int, rs1: int = ZERO):
     instruction = CONDITIONAL
-    instruction |= (0b000 if link else 0b111) << 2
+    instruction |= 0b111 << 2
+    instruction |= bool(absolute) << 6
+    instruction |= get_imm(immediate) | get_rs1(rs1)
+    return instruction
+def jump_link(absolute: bool, immediate: int, rs1: int = ZERO, rd: int = RJL):
+    instruction = CONDITIONAL
     instruction |= bool(absolute) << 6
     instruction |= get_imm(immediate) | get_rs1(rs1) | get_rd(rd)
     return instruction
-def jump_if(condition: int, signed: bool, rs1: int, rd: int, immediate: int = 0):
+def jump_if(condition: int, signed: bool, immediate: int, rs1: int = ZERO, rd: int = ZERO):
     instruction = CONDITIONAL
     instruction |= (int(condition) & 0b111) << 2
     instruction |= (int(signed) & 0b1) << 7
     instruction |= get_rs1(rs1) | get_rd(rd) | get_imm(immediate)
     return instruction
 
-def set_if(condition: int, signed: bool, isimmediate: bool, rs1: int, rs2: int, immediate: int = 0, rd: int = RJL):
+def set_if(condition: int, signed: bool, isimmediate: bool, rd: int, rs1: int, rs2: int = ZERO, immediate: int = 0):
     instruction = CONDITIONAL
     instruction |= (int(condition) & 0b111) << 2
     instruction |= 1 << 5
@@ -47,46 +52,46 @@ def set_if(condition: int, signed: bool, isimmediate: bool, rs1: int, rs2: int, 
     instruction |= get_imm(immediate) if isimmediate else get_rs2(rs2)
     return instruction
 
-def add(isimmediate: bool, carry: bool, rs1: int, rs2: int, rd: int, immediate: int = 0):
+def add(isimmediate: bool, carry: bool, rd: int, rs1: int = ZERO, rs2: int = ZERO, immediate: int = 0):
     instruction = ARITHMETICS
     instruction |= bool(isimmediate) << 2
     instruction |= bool(carry) << 6
     instruction |= get_rs1(rs1) | get_rd(rd)
     instruction |= get_imm(immediate) if isimmediate else get_rs2(rs2)
     return instruction
-def sub(isimmediate: bool, carry: bool, rs1: int, rs2: int, rd: int, immediate: int = 0):
-    instruction = add(isimmediate, carry, rs1, rs2, rd, immediate)
+def sub(isimmediate: bool, carry: bool, rd: int, rs1: int = ZERO, rs2: int = ZERO, immediate: int = 0):
+    instruction = add(isimmediate, carry, rd, rs1, rs2, immediate)
     instruction |= 1 << 5
     return instruction
-def shl(isimmediate: bool, rs1: int, rs2: int, rd: int, immediate: int = 0):
-    instruction = add(isimmediate, False, rs1, rs2, rd, immediate)
+def shl(isimmediate: bool, rd: int, rs1: int = ZERO, rs2: int = ZERO, immediate: int = 0):
+    instruction = add(isimmediate, False, rd, rs1, rs2, immediate)
     instruction |= 1 << 4
     return instruction
-def shr(isimmediate: bool, rs1: int, rs2: int, rd: int, immediate: int = 0):
-    instruction = sub(isimmediate, False, rs1, rs2, rd, immediate)
+def shr(isimmediate: bool, rd: int, rs1: int = ZERO, rs2: int = ZERO, immediate: int = 0):
+    instruction = sub(isimmediate, False, rd, rs1, rs2, immediate)
     instruction |= 1 << 4
     return instruction
-def sha(isimmediate: bool, rs1: int, rs2: int, rd: int, immediate: int = 0):
-    instruction = sub(isimmediate, False, rs1, rs2, rd, immediate)
+def sha(isimmediate: bool, rd: int, rs1: int = ZERO, rs2: int = ZERO, immediate: int = 0):
+    instruction = sub(isimmediate, False, rd, rs1, rs2, immediate)
     instruction |= 1 << 4
     instruction |= 1 << 7
     return instruction
-def bit_and(isimmediate: bool, rs1: int, rs2: int, rd: int, immediate: int = 0):
+def bit_and(isimmediate: bool, rd: int, rs1: int = ZERO, rs2: int = ZERO, immediate: int = 0):
     instruction = ARITHMETICS
     instruction |= bool(isimmediate) << 2
     instruction |= 1 << 3
     instruction |= get_rs1(rs1) | get_rd(rd)
     instruction |= get_imm(immediate) if isimmediate else get_rs2(rs2)
     return instruction
-def bit_or(isimmediate: bool, rs1: int, rs2: int, rd: int, immediate: int = 0):
-    instruction = bit_and(isimmediate, rs1, rs2, rd, immediate)
+def bit_or(isimmediate: bool, rd: int, rs1: int = ZERO, rs2: int = ZERO, immediate: int = 0):
+    instruction = bit_and(isimmediate, rd, rs1, rs2, immediate)
     instruction |= 1 << 4
     return instruction
-def bit_xor(isimmediate: bool, rs1: int, rs2: int, rd: int, immediate: int = 0):
-    instruction = bit_and(isimmediate, rs1, rs2, rd, immediate)
+def bit_xor(isimmediate: bool, rd: int, rs1: int = ZERO, rs2: int = ZERO, immediate: int = 0):
+    instruction = bit_and(isimmediate, rd, rs1, rs2, immediate)
     instruction |= 2 << 4
     return instruction
-def bit_not(isimmediate: bool, rs1: int, rd: int, immediate: int = 0):
-    instruction = bit_and(isimmediate, rs1, ZERO, rd, immediate)
+def bit_not(isimmediate: bool, rd: int, rs1: int = ZERO, immediate: int = 0):
+    instruction = bit_and(isimmediate, rd, rs1, ZERO, immediate)
     instruction |= 3 << 4
     return instruction
